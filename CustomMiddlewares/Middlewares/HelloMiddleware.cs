3a33358c0b9bbaca1dc72404f16dd30103bebe11
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-12-04 09:18:20 +0300

    Son Update

---
+++ CustomMiddlewares/Middlewares/HelloMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomMiddlewares.Middlewares
+{
+    public class HelloMiddleware
+    {
+        //Bir sonraki Middleware temsil eden delegate
+        private readonly RequestDelegate _next;
+        public HelloMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Merhabalar bu bir sorudur..");
+            await _next.Invoke(context);
+            Console.WriteLine("Merhabalar bu bir cevaptır.");
+
+        }
+    }
+}
