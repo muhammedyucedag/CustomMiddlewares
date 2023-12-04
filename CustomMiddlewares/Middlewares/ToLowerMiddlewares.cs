1013ce15a18cee7bfa833c30758549dbde43de2b
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-12-04 08:50:12 +0300

    Created To Lower Middlewares

---
+++ CustomMiddlewares/Middlewares/ToLowerMiddlewares.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomMiddlewares.Middlewares
+{
+    public class ToLowerMiddlewares
+    {
+        //Bir sonraki Middleware temsil eden delegate
+        private readonly RequestDelegate _next;
+        public ToLowerMiddlewares(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Items["value"] != null) 
+            { 
+                var value = context.Items["value"].ToString();
+                context.Items["value"] = value.ToLower();
+            }
+            await _next(context);
+        }
+    }
+}
